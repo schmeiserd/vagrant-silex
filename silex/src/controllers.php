3e1dcf43dcbb26d52893cab41baba8b18c1f096f
schmeiserd <dominik@schmeiser.eu>
2015-01-22 21:32:39 +0100

    add support for plain php as "template engine"

--- silex/src/controllers.php
+++ silex/src/controllers.php
@@ -2,6 +2,13 @@
 use Symfony\Component\HttpFoundation\Request;
 
 $app->get('/welcome/{name}', function ($name) use ($app) {
+    return $app['templating']->render(
+        'hello.html.php',
+        array('name' => $name)
+    );
+});
+
+$app->get('/welcome-twig/{name}', function ($name) use ($app) {
     return $app['twig']->render(
         'hello.html.twig',
         array('name' => $name)
